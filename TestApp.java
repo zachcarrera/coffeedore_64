8f3bd8b6b55149fffbde86193cf3f32730bc1158
Zach Carrera <zachcarrera@gmail.com>
2023-04-04 23:27:53 -0700

    add comments to TestApp.java

--- TestApp.java
+++ TestApp.java
@@ -1,13 +1,15 @@
 public class TestApp {
     public static void main(String[] args) {
         CoffeeKiosk kiosk1 = new CoffeeKiosk();
+
+        // fill menu
         kiosk1.addMenuItem("banana", 2);
         kiosk1.addMenuItem("coffee", 1.5);
         kiosk1.addMenuItem("latte", 4.5);
         kiosk1.addMenuItem("cappuccino", 3);
         kiosk1.addMenuItem("muffin", 4);
 
-
+        // start a new order on kiosk1
         kiosk1.newOrder();
     }
 }
